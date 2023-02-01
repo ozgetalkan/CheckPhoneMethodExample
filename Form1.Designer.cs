39bec1d98f895d0051c5aa0d175f32940f19170d
OzgeTanulku <ozgetanulkuu@gmail.com>
2023-02-01 07:11:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheckPhone = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCheckPhone
+            // 
+            this.btnCheckPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckPhone.Location = new System.Drawing.Point(180, 194);
+            this.btnCheckPhone.Name = "btnCheckPhone";
+            this.btnCheckPhone.Size = new System.Drawing.Size(157, 42);
+            this.btnCheckPhone.TabIndex = 0;
+            this.btnCheckPhone.Text = "Kontrol Et";
+            this.btnCheckPhone.UseVisualStyleBackColor = true;
+            this.btnCheckPhone.Click += new System.EventHandler(this.btnCheckPhone_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phone:";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhoneNumber.Location = new System.Drawing.Point(180, 110);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(202, 31);
+            this.txtPhoneNumber.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 411);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheckPhone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheckPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckPhone_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = txtPhoneNumber.Text;
+            string phoneLast = CheckPhone(phoneNumber);
+            if (phoneLast.Contains("Hata"))
+            {
+                MessageBox.Show(phoneLast);
+            }
+            else
+            {
+                MessageBox.Show($"{ phoneLast } telefona mesaj gonderilmistir.");
+            }
+        }
+        public string CheckPhone(string phone)
+        {
+            try
+            {
+                if (phone == "")
+                {
+                    return "Hata mesaji => l]tfen telefon alanini doldurun";
+                }
+                phone = phone.Replace(" ", "");
+                phone = phone.Replace("+", "");
+                string firstCharacter = phone.Substring(0, 1);
+                if (firstCharacter == "9")
+                {
+                    phone = phone.Substring(1, phone.Length - 1);
+                }
+                else if (firstCharacter != "0")
+                {
+                    phone = "0" + phone;
+                }
+                if (phone.Length == 11)
+                {
+                    Convert.ToDouble(phone);
+                    return phone;
+                }
+                return "Hata mesaji. Lutfen telefon formatini kontrol ediniz. 11 hane olmalidir";
+            }
+            catch (FormatException)
+            {
+
+                return "Hata mesaji => L]tfen uygun format ile telefon giriniz";
+            }
+            catch(Exception)
+            {
+                return "Hata mesaji => Bilinmeyen hata lutfen telefon numarasini tekrar giriniz";
+            }
+        }
+    }
+}
